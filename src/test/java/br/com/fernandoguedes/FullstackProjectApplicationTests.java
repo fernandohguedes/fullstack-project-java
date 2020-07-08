0ef09873b825d6a995fda9e45af3047dcac87880
Fernando Guedes <fernando_guedes@beirario.com.br>
2020-07-07 22:15:20 -0300

    commit inicial

---
+++ src/test/java/br/com/fernandoguedes/FullstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandoguedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
